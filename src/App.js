1c06a9bf51bb0a75e457960a42baf230148c23a9
suraj sanjay kokane <108039887+kokanesuraj007@users.noreply.github.com>
2023-04-30 11:24:19 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import Home from "./Components/Home";
+import Navbar from "./Components/Navbar";
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <Navbar/>
+      <Home/>
+    </div>
+  );
+}
